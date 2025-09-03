406785f5b6c4c86c89307d8bd241eddca60d2b1b
seweed3 <142646489+seweed3@users.noreply.github.com>
2025-09-02 17:10:52 -0700

    Update A_OpModeLExample.java
    Add "random stuff"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Examples/A_OpModeLExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Examples/A_OpModeLExample.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp //This determines what kind of program you are using (TeleOp or Auto). TeleOp is better for experimentation because Auto ends the program after 30 seconds
 public class A_OpModeLExample extends OpMode { //Make sure to put "extends OpMode"
-    //There are 2 phases in an FTC program. Initialize and Run Mode
+    //There are 2 phases in an FTC program. Initialize and Run Mode Random stuff
 
     @Override //Honestly idk exactly what it does, but its kinda important to make sure to have it before a method
     public void init(){ // This function is what happens during initialization. Used for setting up sensors, motors, etc
